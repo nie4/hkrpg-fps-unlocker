f2d6adb1b34d76f17613ecd6582e40e2a72082e4
Nie <191497838+nie4@users.noreply.github.com>
2025-07-04 13:00:58 +0200

    Update for v3.4.0

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # hkrpg-fps-unlocker
-FPS unlocker for StarRail ```OSPROD v3.3.0```
+FPS unlocker for StarRail ```OSPROD v3.4.0```
 
 ## Requirements:
 - [Rust](https://www.rust-lang.org/tools/install)
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,23 @@
 use ctor::ctor;
-use std::time::Duration;
-use windows::core::s;
-use windows::Win32::System::LibraryLoader::GetModuleHandleA;
+use std::{thread, time::Duration};
+use windows::{Win32::System::LibraryLoader::GetModuleHandleA, core::s};
 
-const OFFSET: usize = 0x1D0A710;
-
-// Change this value
+const OFFSET: usize = 0x1D39750;
 const TARGET_FRAMERATE: i32 = 144;
 
-unsafe fn unlocker_thread() {
-    let unity_player = loop {
-        match GetModuleHandleA(s!("UnityPlayer.dll")) {
-            Ok(handle) => break handle.0 as usize,
-            Err(_) => std::thread::sleep(Duration::from_millis(100)),
-        }
-    };
-
-    let fps_value = unity_player.wrapping_add(OFFSET) as *mut i32;
-
-    loop {
-        std::thread::sleep(Duration::from_secs(5));
-        *fps_value = TARGET_FRAMERATE;
-    }
-}
-
 #[ctor]
-unsafe fn main() {
-    std::thread::spawn(|| unlocker_thread());
+fn unlocker() {
+    thread::spawn(|| {
+        let base = loop {
+            if let Ok(handle) = unsafe { GetModuleHandleA(s!("UnityPlayer.dll")) } {
+                break handle.0 as usize;
+            }
+            thread::sleep(Duration::from_secs(1));
+        };
+        let fps_ptr = (base + OFFSET) as *mut i32;
+        loop {
+            thread::sleep(Duration::from_secs(5));
+            unsafe { *fps_ptr = TARGET_FRAMERATE; }
+        }
+    });
 }
